f10b6d4a8fd32207d825f6822fbccae4f156a321
Imtiaz <imtcse@gmail.com>
2019-04-04 03:31:24 +0600

    User Signup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -13,7 +13,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login','signup']]);
     }
 
     /**
@@ -31,6 +31,12 @@ class AuthController extends Controller
 
         return $this->respondWithToken($token);
     }
+    public function signup(Request $request)
+    {
+       
+        User::create($request->all());
+        return $this->login($request);
+    } 
 
     /**
      * Get the authenticated User.
